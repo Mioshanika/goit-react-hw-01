c2bbe5be0f38cd6db29016ec3a3bd05fac6defa8
Miosha Tala <miosha.tala@gmail.com>
2025-03-19 18:24:21 +0200

    main.css fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './main.module.css';
+import './main.css';
 import App from './components/app/app.jsx';
 
 createRoot(document.getElementById('root')).render(
