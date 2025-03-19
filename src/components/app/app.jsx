3ef592d30d9857498b975cc11b4851435ddcfdf6
Miosha Tala <miosha.tala@gmail.com>
2025-03-19 18:06:32 +0200

    done

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,26 @@
-import './app.module.css';
+// Css
+import appCss from './app.module.css';
+// Data
+import userData from '/src/data/userdata.json';
+import friends from '/src/data/friends.json';
+import transactions from '/src/data/transactions.json';
+// Components
+import Profile from '../profile/profile.jsx';
+import FriendList from '../friendlist/friendlist.jsx';
+import TransactionHistory from '../transactionhistory/transactionhistory.jsx';
 
 export default function App() {
   return (
-    <>
-      <p>Hello, World!</p>
-    </>
+    <div className={appCss.app_container}>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
   );
 }
